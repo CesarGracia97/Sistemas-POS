392b5167c20f1b3ba692c967f741aa1d93e307a1
CESAR GRACIA <cesar.gracia@pucese.edu.ec>
2023-04-12 09:36:17 -0500

    Sistema-Pos Bases

---
+++ src/layout/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Layout = () =>{
+    return (
+        <div>
+            <header className='bg-gray-900 shadow'>
+                <div className='mx-auto py-6 px-4'>
+                    <h1 className='text-white font-bold text-3x1'>DataTable</h1>
+                </div>
+            </header>
+            <main>
+                <div className='mx-auto py-6'>
+                    contenido
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Layout
